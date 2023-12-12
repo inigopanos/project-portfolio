e91643b96b264bb2183d157722d7400424438d74
Iñigo <inigopanos@gmail.com>
2023-12-12 17:30:45 +0100

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../components/footer-component/footer-component.component';
+import { ProjectComponent } from '../components/project-component/project-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
