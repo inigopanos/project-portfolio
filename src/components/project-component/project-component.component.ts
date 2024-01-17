8a60a88251f40d6471ff632da5d947385b294e55
Iñigo <inigopanos@gmail.com>
2024-01-17 12:37:35 +0100

    Cambios en css

--- src/components/project-component/project-component.component.ts
+++ src/components/project-component/project-component.component.ts
@@ -20,13 +20,13 @@ export class ProjectComponent {
         image: '/assets/images/columna.png',
         description: 'MEVN stack project, in which you can create, update, read and delete ruins, as well as register and login as users. Tested on backend using Jest.',
         link: 'https://vaya-ruina.netlify.app',
-        skills: ['Vue.js', 'Web Design', 'Typescript', 'Node.js', 'MongoDB', 'Jest'],
+        skills: ['Vue.js', 'WeDev', 'Typescript', 'Node.js', 'MongoDB', 'Jest'],
         githubFrontend: 'https://github.com/inigopanos/Inigo-Panos_Front-Final-Project-202201-MAD',
         githubBackend: 'https://github.com/inigopanos/Inigo-Panos_Back-Final-Project-202201-MAD',
       },
-      {
+      {   
         title: 'Albums',
-        image: '/assets/images/columna.png',
+        image: '/assets/images/album.png',
         description: 'MEAN stack project, in which you can create, update, read and delete albums.',
         link:'holahola',
         skills: ['Angular', 'Node.js', 'Typescript', 'Node.js', 'MongoDB']
